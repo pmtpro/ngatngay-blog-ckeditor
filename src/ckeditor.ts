b141ac93b9555b8b63a7b7d90bd06a196462af31
ngatngay <ngatngay@ngatngay.net>
2024-03-10 15:28:50 +0700

    #1

---
+++ src/ckeditor.ts
@@ -0,0 +1,185 @@
+/**
+ * @license Copyright (c) 2014-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import {
+	Bold,
+	Code,
+	Italic,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Underline
+} from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import {
+	Image,
+	ImageCaption,
+	ImageInsert,
+	ImageToolbar,
+	ImageUpload,
+	ImageStyle
+} from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { LegacyList, LegacyListProperties, LegacyTodoList } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
+import { PageBreak } from '@ckeditor/ckeditor5-page-break';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
+import {
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableToolbar
+} from '@ckeditor/ckeditor5-table';
+import { AccessibilityHelp } from '@ckeditor/ckeditor5-ui';
+import { Undo } from '@ckeditor/ckeditor5-undo';
+import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		AccessibilityHelp,
+		Alignment,
+		BlockQuote,
+		Bold,
+		Code,
+		CodeBlock,
+		Essentials,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
+		Heading,
+		Image,
+		ImageCaption,
+		ImageInsert,
+		ImageToolbar,
+		ImageUpload,
+		ImageStyle,
+		Indent,
+		IndentBlock,
+		Italic,
+		LegacyList,
+		LegacyListProperties,
+		LegacyTodoList,
+		Link,
+		LinkImage,
+		MediaEmbed,
+		MediaEmbedToolbar,
+		PageBreak,
+		Paragraph,
+		RemoveFormat,
+		SimpleUploadAdapter,
+		SourceEditing,
+		Strikethrough,
+		Subscript,
+		Superscript,
+		Table,
+		TableCaption,
+		TableCellProperties,
+		TableColumnResize,
+		TableToolbar,
+		Underline,
+		Undo
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'undo',
+				'redo',
+				'|',
+				'heading',
+				'|',
+				'imageInsert',
+				'link',
+				'bold',
+				'italic',
+				'underline',
+				'blockQuote',
+				'code',
+				'codeBlock',
+				'|',
+				'alignment',
+				'outdent',
+				'indent',
+				'|',
+				'bulletedList',
+				'numberedList',
+				'todoList',
+				'mediaEmbed',
+				'insertTable',
+				'|',
+				'pageBreak',
+				'fontBackgroundColor',
+				'fontColor',
+				'fontFamily',
+				'fontSize',
+				'subscript',
+				'superscript',
+				'strikethrough',
+				'|',
+				'removeFormat',
+				'sourceEditing',
+				'accessibilityHelp'
+			]
+		},
+		language: 'en',
+		image: {
+			toolbar: [
+				'insertImage',
+
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'linkImage',
+				
+				'imageStyle:inline',
+				
+				'imageStyle:block',
+				'imageStyle:alignCenter',
+				'imageStyle:alignBlockLeft',
+				'imageStyle:alignBlockRight',
+				
+				'imageStyle:side',
+				'imageStyle:alignLeft',
+				'imageStyle:alignRight'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells',
+				'tableCellProperties'
+			]
+		},
+		heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+            ]
+        }
+	};
+}
+
+export default Editor;
